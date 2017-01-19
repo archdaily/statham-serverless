7931a220ece274e126da1fcd9929e6228a2b371b
Daniel Molina <dmolinae@outlook.com>
2017-01-19 14:48:00 -0300

    FIX: fixed duplicated issue

--- handler.js
+++ handler.js
@@ -40,73 +40,60 @@ module.exports.sendMessage = (event, context, callback) => {
   };
 
   var req = https.request(options, (res) => {
-    responseMsg.data = "";
+    var data = "";
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
       console.log(`DATA CHUNK: ${chunk}`);
-      responseMsg.data += chunk;
+      data += chunk;
     });
     res.on('end', () => {
-      console.log(`DATA: ${responseMsg.data}`);
-      responseMsg.data = JSON.parse(responseMsg.data);
-      responseMsg.emit('success');
+      var dataJSON = JSON.parse(data);
+
+      var response = {
+      statusCode: 200,
+      body: JSON.stringify({
+          "Success" : dataJSON
+          })
+      };
+
+      callback(null, response);
     });
   });
 
   req.on('error', (e) => {
-    responseMsg.error = `problem with request: ${e.message}`;
-    console.log(responseMsg.error);
-    responseMsg.emit('error');
-  });
-
-  req.write(postData);
-  req.end();
-
-  responseMsg.on('success', function () {
-    var response = {
-      statusCode: 200,
-      body: JSON.stringify({
-          "Success" : responseMsg.data
-          })
-    };
-
-    callback(null, response);
-  });
-
-
-  responseMsg.on('error', function () {
+    var error = `ERROR: ${e.message}`;
+    
     AWS.config.update({accessKeyId: 'A***REMOVED***', secretAccessKey: '***REMOVED***'});
     var sqs = new AWS.SQS("us-west-2");
-
-    responseMsg.sqs = "";
 
     var sqsParams = {
       MessageBody: JSON.stringify(messageJSON),
       QueueUrl: 'https://sqs.us-west-2.amazonaws.com/451967854914/Statham-trunk'
     };
     sqs.sendMessage(sqsParams, function(err, data) {
+      var responseSQS = "";
       if (err) {
         console.log('STORESQSERROR:', err);
-        responseMsg.sqs = responseMsg.sqs + 'STORESQSERROR: ' + err + ' ';
-        responseMsg.emit('sqsResponse');
+        responseSQS = responseSQS + 'STORESQSERROR: ' + err + ' ';
       }
       else{
         console.log(data);
-        responseMsg.sqs = responseMsg.sqs + 'DATA: ' + data + ' ';
-        responseMsg.emit('sqsResponse');
+        responseSQS = responseSQS + 'DATA: ' + data + ' ';
       }
-    });
-    responseMsg.on('sqsResponse', function(){
       var response = {
         statusCode: 400,
         body: JSON.stringify({
-            "Error" : responseMsg.error,
-            "SQSResponse" : responseMsg.sqs
+            "Error" : error,
+            "SQSResponse" : responseSQS
         })
       };
+
       callback(null, response);
     });
   });
+
+  req.write(postData);
+  req.end();
 };
 
 module.exports.receiver = (event, context, callback) => {
