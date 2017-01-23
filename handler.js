0b44d361156ef8674090fd9962da36fe7bc62a6b
caoomo <caoomo@gmail.com>
2017-01-23 17:32:27 -0300

    FIX: indentation 0.2

--- handler.js
+++ handler.js
@@ -77,71 +77,71 @@ var error_message_to_email = function(messageJSON){
 var send_message = function(messageJSON){
   if(messageJSON.tries > 1) sleep(10000); //fixed to 10 seconds 
 
-    var postData = JSON.stringify(messageJSON.body);
+  var postData = JSON.stringify(messageJSON.body);
 
-    var urlDest = url.parse(messageJSON.url);
+  var urlDest = url.parse(messageJSON.url);
 
-    messageJSON.dest = urlDest.pathname;
+  messageJSON.dest = urlDest.pathname;
 
-    var options = {
-      hostname: urlDest.host,
-      port: urlDest.port,
-      path: urlDest.pathname,
-      method: messageJSON.method,
-      headers: {
-        'Content-Type' : 'application/json',
-        'Content-Length': postData.length
-      }
-    };
+  var options = {
+    hostname: urlDest.host,
+    port: urlDest.port,
+    path: urlDest.pathname,
+    method: messageJSON.method,
+    headers: {
+      'Content-Type' : 'application/json',
+      'Content-Length': postData.length
+    }
+  };
 
-    var req = https.request(options, (res) => {
-      var data = "";
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => {
-        data += chunk;
-      });
-      res.on('end', () => {
-        var dataJSON = JSON.parse(data);
-        var response = {
-          statusCode: 200,
-          body: JSON.stringify({
-            "Success" : dataJSON
-            })
-        };
-        callback(null, response);
-      });
+  var req = https.request(options, (res) => {
+    var data = "";
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      data += chunk;
     });
-
-    req.on('error', (e) => {
-      var error = `ERROR: ${e.message}`;
-
-      messageJSON.error = error;
-
-      var snsParams = {
-        Message: JSON.stringify(messageJSON),
-        Subject: "Message not delivered From Lambda",
-        TopicArn: 'arn:aws:sns:us-west-2:451967854914:Statham-notification'
-      };
-      sns.publish(snsParams, function(errSNS, dataSNS){
-        var responseSNS = "";
-        if(errSNS){
-          responseSNS = responseSNS + 'SENDSNSERROR: ' + errSNS + ' ';
-        }
-        else{
-          responseSNS = responseSNS + 'DATA: ' + dataSNS + ' ';
-        }
-        var response = {
-          statusCode: 400,
-          body: JSON.stringify({
-              "Error" : error,
-              "SNSResponse" : responseSNS
+    res.on('end', () => {
+      var dataJSON = JSON.parse(data);
+      var response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          "Success" : dataJSON
           })
-        };
-        callback(null, response);
-      });
+      };
+      callback(null, response);
     });
-    req.write(postData);
-    req.end();
+  });
+
+  req.on('error', (e) => {
+    var error = `ERROR: ${e.message}`;
+
+    messageJSON.error = error;
+
+    var snsParams = {
+      Message: JSON.stringify(messageJSON),
+      Subject: "Message not delivered From Lambda",
+      TopicArn: 'arn:aws:sns:us-west-2:451967854914:Statham-notification'
+    };
+    sns.publish(snsParams, function(errSNS, dataSNS){
+      var responseSNS = "";
+      if(errSNS){
+        responseSNS = responseSNS + 'SENDSNSERROR: ' + errSNS + ' ';
+      }
+      else{
+        responseSNS = responseSNS + 'DATA: ' + dataSNS + ' ';
+      }
+      var response = {
+        statusCode: 400,
+        body: JSON.stringify({
+            "Error" : error,
+            "SNSResponse" : responseSNS
+        })
+      };
+      callback(null, response);
+    });
+  });
+  req.write(postData);
+  req.end();
 }
 
 module.exports.sendMessage = (event, context, callback) => {
