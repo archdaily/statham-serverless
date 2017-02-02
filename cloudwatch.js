d7f90570d84ac523a2bd2c511fadb519f7042161
Daniel Molina <dmolinae@outlook.com>
2017-02-02 18:28:25 -0300

    ADD: add permission on lambda to be triggered by Schedule

--- cloudwatch.js
+++ cloudwatch.js
@@ -5,6 +5,7 @@ var Key_Id            = 'A***REMOVED***';
 var secretAccessKey   = '***REMOVED***';
 AWS.config.update({accessKeyId: Key_Id, secretAccessKey: secretAccessKey});
 var cloudwatchevents  = new AWS.CloudWatchEvents();
+var lambda            = new AWS.Lambda();
 
 module.exports.enable_rule = function(){
   var params = {
@@ -16,6 +17,7 @@ module.exports.enable_rule = function(){
     if (err) console.log(err, err.stack);
     else{
       put_lambda_target();
+      add_permission_trigger_lambda(data.RuleArn);
     }
   });
 }
@@ -37,11 +39,25 @@ var put_lambda_target = function(){
     Targets: [
       {
         Arn: "arn:aws:lambda:us-west-2:451967854914:function:msgService-dev-send",
-        Id: "1"
+        Id: "Id243536924640"
       }
     ]
   };
   cloudwatchevents.putTargets(params, function(err, data) {
     if (err) console.log(err, err.stack);
   });
+}
+
+var add_permission_trigger_lambda = function(ruleArn){
+  var params = {
+    Action: "lambda:InvokeFunction",
+    FunctionName: "arn:aws:lambda:us-west-2:451967854914:function:msgService-dev-send",
+    Principal: "events.amazonaws.com",
+    SourceArn: ruleArn,
+    StatementId: "ID-1"
+  };
+  lambda.addPermission(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+});
+
 }
