465e437bd67cb360796aea45908147f87a65e4be
Daniel Molina <dmolinae@outlook.com>
2017-02-07 17:32:13 -0300

    REFACT: receiver repeated code

--- src/receiver.js
+++ src/receiver.js
@@ -5,43 +5,41 @@ var utilities   = require('utilities');
 var cloudwatch  = require('cloudwatch');
 
 module.exports.receiveAndSendMessage = (event, context, callback) => {
-
   var messageJSON = utilities.fetch_request_message(event);
-
   Message.send(messageJSON, function(sent){
-    if (messageJSON.email){
-      if(sent){
-        utilities.make_html_response(function(response){
-          callback(null,response);
-        }, "The message was delivered successfully."
-        );
-      }
-      else{
-        cloudwatch.enable_rule();
-        utilities.make_html_response(function(response){
-          callback(null,response);
-        }, "The message could not be delivered but is in the queue of attempts."
-        );
-      }
+
+    if(sent){
+      send_response(
+        messageJSON.email,
+        "The message was delivered successfully."
+      );
     }
     else{
-      if(sent){
-        callback(null, endpoint_response("Message sent."));
-      }
-      else{
-        cloudwatch.enable_rule();
-        callback(null, endpoint_response(
-          "The message could not be delivered but is in the queue of attempts."
-        ));
-      }
+      send_response(
+        messageJSON.email,
+        "The message could not be delivered but is in the queue of attempts."
+      );
     }
+
   });
 }
 
-var endpoint_response = function(status){
+var send_response = function(email, message){
+  if(email == 1){
+    utilities.make_html_response(function(response){
+      callback(null,response);
+    },
+    message);
+  }
+  else{
+    callback(null, endpoint_response(message));
+  }
+}
+
+var endpoint_response = function(message){
   var response = utilities.make_json_response(200,{
     "Response" : "Statham received your message!",
-    "Status" : status
+    "Status" : message
   });
   return response;
 }
