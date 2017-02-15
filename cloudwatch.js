80b0f4b7f5f0b1b0f0833d07795668d4d9847732
Daniel Molina <dmolinae@outlook.com>
2017-02-15 16:11:07 -0300

    FIX: way to link rule to lambda worker FIX: name of variable on receiver

--- cloudwatch.js
+++ cloudwatch.js
@@ -11,21 +11,13 @@ var scheduleExpression  = config.get('CycleExpression');
 var StathamRuleName     = "StathamCycle";
 
 module.exports.enable_rule = function(){
-  exist_rule(function(exist){
-    var params = {
-      Name: StathamRuleName,
-      ScheduleExpression: scheduleExpression,
-      State: 'ENABLED'
-    };
-    cloudwatchevents.putRule(params, function(err, data) {
-      if (err) console.log(err, err.stack);
-      else{
-        if(!exist){
-          put_lambda_target();
-          add_permission_trigger_lambda(data.RuleArn);
-        }
-      }
-    });
+  var params = {
+    Name: StathamRuleName,
+    ScheduleExpression: scheduleExpression,
+    State: 'ENABLED'
+  };
+  cloudwatchevents.putRule(params, function(err, data) {
+    if (err) console.log(err, err.stack);
   });
 }
 
@@ -37,38 +29,6 @@ module.exports.disable_rule = function(){
   };
   cloudwatchevents.putRule(params, function(err, data) {
     if (err) console.log(err, err.stack);
-  });
-}
-
-var put_lambda_target = function(){
-  get_lambda(function(LambdaArn){
-    var params = {
-      Rule: StathamRuleName,
-      Targets: [
-        {
-          Arn: LambdaArn,
-          Id: "Lambda-Worker"
-        }
-      ]
-    };
-    cloudwatchevents.putTargets(params, function(err, data) {
-      if (err) console.log(err, err.stack);
-    });
-  });
-}
-
-var add_permission_trigger_lambda = function(ruleArn){
-  get_lambda(function(LambdaArn){
-    var params = {
-      Action: "lambda:InvokeFunction",
-      FunctionName: LambdaArn,
-      Principal: "events.amazonaws.com",
-      SourceArn: ruleArn,
-      StatementId: "ID-1"
-    };
-    lambda.addPermission(params, function(err, data) {
-      if (err) console.log(err, err.stack);
-    });
   });
 }
 
@@ -86,16 +46,5 @@ var exist_rule = function(callback){
       if(data.Rules.length == 1) callback(true);
       else callback(false);
     }
-  });
-}
-
-var get_lambda = function(callback){
-  var params = {
-    FunctionName: "StathamService-dev-worker"
-  };
-  lambda.getFunction(params, function(err, data) {
-    if (err) console.log(err, err.stack);
-    else
-      callback(data.Configuration.FunctionArn);
   });
 }
--- src/receiver.js
+++ src/receiver.js
@@ -13,13 +13,13 @@ module.exports.emailResend = (event, context, callback) => {
   validate_and_send(true, event, callback);
 }
 
-var validate_and_send = function(email, event, callback) {
-  var messageJSON = validator.validateParams(email, event);
+var validate_and_send = function(isFromEmail, event, callback) {
+  var messageJSON = validator.validateParams(isFromEmail, event);
   if (messageJSON) {
     messageJSON = utilities.add_extras(event, messageJSON);
-    deliver_message(email, messageJSON, callback);
+    deliver_message(isFromEmail, messageJSON, callback);
   } else {
-    utilities.create_response(email, "There is an error with the request. Please verify.",
+    utilities.create_response(isFromEmail, "There is an error with the request. Please verify.",
       function(response) {
         callback(null, response);
       }
@@ -27,17 +27,17 @@ var validate_and_send = function(email, event, callback) {
   }
 }
 
-var deliver_message = function(email, messageJSON, callback) {
+var deliver_message = function(isHTML, messageJSON, callback) {
   Message.send(messageJSON, function(sent) {
     if (sent) {
-      create_response(email, "The message was delivered successfully.",
+      create_response(isHTML, "The message was delivered successfully.",
         function(response) {
           callback(null, response);
         }
       );
     } else {
       cloudwatch.enable_rule();
-      utilities.create_response(email,
+      utilities.create_response(isHTML,
         "The message couldn't be sent, therefore it was added to the queue",
         function(response) {
           callback(null, response);
