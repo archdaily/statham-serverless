3ff23c823f336d86bc133737d24b16ca08775c66
Daniel Molina <dmolinae@outlook.com>
2017-02-15 10:59:35 -0300

    REFACT: Beauty and create_response changed to utilities.

--- utilities.js
+++ utilities.js
@@ -1,12 +1,12 @@
 'use strict';
-var fs                = require('fs');
-var ejs               = require('ejs');
-var url               = require('url');
-var jwt               = require('jsonwebtoken');
-var moment            = require('moment');
+var fs = require('fs');
+var ejs = require('ejs');
+var url = require('url');
+var jwt = require('jsonwebtoken');
+var moment = require('moment');
 
-var credentials       = require('nconf').file('credentials.json');
-var secretToken       = credentials.get('secretToken');
+var credentials = require('nconf').file('credentials.json');
+var secretToken = credentials.get('secretToken');
 
 module.exports.createToken = function(origin) {
   var payload = {
@@ -17,7 +17,7 @@ module.exports.createToken = function(origin) {
   return jwt.sign(payload, secretToken);
 };
 
-module.exports.make_json_response = function(callback, statusCode, body){
+module.exports.make_json_response = function(callback, statusCode, body) {
   var response = {
     statusCode: statusCode,
     body: JSON.stringify(body)
@@ -25,18 +25,18 @@ module.exports.make_json_response = function(callback, statusCode, body){
   callback(response);
 }
 
-module.exports.add_extras = function(event, messageJSON){
+module.exports.add_extras = function(event, messageJSON) {
   var urlDest = url.parse(messageJSON.url);
   messageJSON.destination = urlDest.pathname;
   messageJSON.source = event.headers.Origin;
-  if(!messageJSON.source) messageJSON.source = "email";
+  if (!messageJSON.source) messageJSON.source = "email";
   messageJSON.resource = event.headers["X-Forwarded-Proto"] + "://" + event.headers["Host"] + "/" + event.requestContext["stage"] + event.path;
   messageJSON.id = event.requestContext.requestId;
   return messageJSON;
 }
 
-module.exports.make_html_response = function(message, callback){
-  message_html(message, function(data){
+module.exports.make_html_response = function(message, callback) {
+  message_html(message, function(data) {
     var response = {
       statusCode: 200,
       headers: {
@@ -48,18 +48,26 @@ module.exports.make_html_response = function(message, callback){
   });
 }
 
-module.exports.get_random_char = function(){
+module.exports.create_response = function(email, message, callback) {
+  if (email) {
+    utilities.make_html_response(message, callback);
+  } else {
+    utilities.make_json_response(callback, 200, { "Status": message });
+  }
+}
+
+module.exports.get_random_char = function() {
   var charSet = char_set();
   var char = charSet.charAt(get_random_number(0, charSet.length));
   return char;
 }
 
-var get_random_number = function(lowerBound,upperBound){
+var get_random_number = function(lowerBound, upperBound) {
   var random = Math.floor(Math.random() * (upperBound - lowerBound)) + lowerBound;
   return random;
 }
 
-var char_set = function(){
+var char_set = function() {
   var charSet = "";
   charSet += number_chars();
   charSet += lower_chars();
@@ -67,25 +75,25 @@ var char_set = function(){
   return charSet;
 }
 
-var lower_chars = function(){
+var lower_chars = function() {
   return "abcdefghijklmnopqrstuvwxyz";
 }
 
-var upper_chars = function(){
+var upper_chars = function() {
   return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }
 
-var number_chars = function(){
+var number_chars = function() {
   return "0123456789";
 }
 
-var message_html = function(message, callback){
-  fs.readFile('views/resend.html', 'utf8', function (err,data) {
+var message_html = function(message, callback) {
+  fs.readFile('views/resend.html', 'utf8', function(err, data) {
     if (err) {
       console.log(err);
     }
     var data_message = ejs.render(data, {
-        message      : message
+      message: message
     });
     callback(data_message);
   });
