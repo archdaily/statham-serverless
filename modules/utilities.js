ae19dd13b6b220f28bd2d03f22c8a01dbf8a9e19
Daniel Molina <dmolinae@outlook.com>
2017-02-20 17:52:42 -0300

    DEL: useless logs

--- modules/utilities.js
+++ modules/utilities.js
@@ -103,7 +103,7 @@ var number_chars = function() {
 var message_html = function(message, callback) {
   fs.readFile('views/resend.html', 'utf8', function(err, data) {
     if (err) {
-      console.log(err);
+      console.log();
     }
     var data_message = ejs.render(data, {
       message: message
